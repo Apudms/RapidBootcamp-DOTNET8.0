f2a2db960665593309874d427458806732131c9b
Muhammad Saefudin <Muhammad.Saefudin@rapidtech.id>
2024-06-10 22:58:58 +0700

    Modify ProductsController

--- RapidBootcamp.BackendAPI/Controllers/ProductsController.cs
+++ RapidBootcamp.BackendAPI/Controllers/ProductsController.cs
@@ -17,6 +17,7 @@ namespace RapidBootcamp.BackendAPI.Controllers
             _product = product;
         }
 
+        #region Get All
         // GET: api/<ProductsController>
         [HttpGet]
         public IEnumerable<Product> Get()
@@ -24,7 +25,9 @@ namespace RapidBootcamp.BackendAPI.Controllers
             var products = _product.GetAll();
             return products;
         }
+        #endregion
 
+        #region Get By ID
         // GET api/<ProductsController>/5
         [HttpGet("{id}")]
         public Product Get(int id)
@@ -32,7 +35,9 @@ namespace RapidBootcamp.BackendAPI.Controllers
             var product = _product.GetById(id);
             return product;
         }
+        #endregion
 
+        #region Get By Name
         // GET api/<ProductsController>/5
         [HttpGet("ByName")]
         public IEnumerable<Product> GetByName(string name)
@@ -40,7 +45,29 @@ namespace RapidBootcamp.BackendAPI.Controllers
             var product = _product.GetByProductName(name);
             return product;
         }
+        #endregion
 
+        #region Get By Category ID
+        // GET api/<ProductsController>/5
+        [HttpGet("ByCategory/{id}")]
+        public IEnumerable<Product> GetByCategoryId(int categoryId)
+        {
+            var product = _product.GetByCategoryId(categoryId);
+            return product;
+        }
+        #endregion
+
+        #region Get By Category Name
+        // GET api/<ProductsController>/5
+        [HttpGet("ByCategoryName")]
+        public IEnumerable<Product> GetByCategoryName(string categoryName)
+        {
+            var product = _product.GetByCategoryName(categoryName);
+            return product;
+        }
+        #endregion
+
+        #region Get By Product with Category
         // GET api/<ProductsController>/5
         [HttpGet("WithCategory")]
         public IEnumerable<Product> GetProductWithCategory()
@@ -48,7 +75,9 @@ namespace RapidBootcamp.BackendAPI.Controllers
             var product = _product.GetProductsWithCategory();
             return product;
         }
+        #endregion
 
+        #region Post (Add New Product)
         // POST api/<ProductsController>
         [HttpPost]
         public ActionResult Post(Product product)
@@ -63,32 +92,54 @@ namespace RapidBootcamp.BackendAPI.Controllers
                 return BadRequest(ex.Message);
             }
         }
+        #endregion
 
+        #region Put (Update Product)
         // PUT api/<ProductsController>/5
         [HttpPut("{id}")]
         public ActionResult Put(int id, [FromBody] Product product)
         {
             var updateProd = _product.GetById(id);
-            if (updateProd != null)
+            if (updateProd == null)
+            {
+                return NotFound();                            
+            }
+            try 
             {
                 updateProd.ProductName = product.ProductName;
                 updateProd.CategoryId = product.CategoryId;
                 updateProd.Stock = product.Stock;
                 updateProd.Price = product.Price;
-                var result Ok(result);
+                var result = _product.Update(updateProd);
                 return Ok(result);
             }
-            else 
+            catch (Exception ex)
             {
-                return NotFound();                            
+                return BadRequest(ex.Message);
             }
-            
         }
+        #endregion
 
+        #region Delete Product
         // DELETE api/<ProductsController>/5
         [HttpDelete("{id}")]
-        public void Delete(int id)
+        public ActionResult Delete(int id)
         {
+            var deleteProd = _product.GetById(id);
+            if ( deleteProd == null )
+            {
+                return NotFound();
+            }
+            try
+            {
+                _product.Delete(id);
+                return Ok($"Product {id} berhasil dihapus!");
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
         }
+        #endregion
     }
 }
